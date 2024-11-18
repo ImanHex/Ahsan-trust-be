9cde6f27d729e9301aba3826afe6bd3955663a39
nooriman <nurfarahrohadatulaisyah@gmail.com>
2024-11-18 21:36:19 +0900

    Revert "add fe vercel"
    This reverts commit 557de79a739b9f1fa4ddf8fe1ba76d955b6a68b5.

--- ahsantrust/ahsantrust/settings.py
+++ ahsantrust/ahsantrust/settings.py
@@ -60,11 +60,10 @@ MIDDLEWARE = [
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
-
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",  # Local development
-    "https://ahsan-trust-fe.vercel.app",  # Deployed React app
+    "http://localhost:5173",
 ]
+
 
 ROOT_URLCONF = "ahsantrust.urls"
 
