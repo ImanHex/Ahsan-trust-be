557de79a739b9f1fa4ddf8fe1ba76d955b6a68b5
nooriman <nurfarahrohadatulaisyah@gmail.com>
2024-11-18 20:05:27 +0900

    add fe vercel

--- ahsantrust/ahsantrust/settings.py
+++ ahsantrust/ahsantrust/settings.py
@@ -60,10 +60,11 @@ MIDDLEWARE = [
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Local development
+    "https://ahsan-trust-fe.vercel.app",  # Deployed React app
+]
 
 ROOT_URLCONF = "ahsantrust.urls"
 
